dfc443b5358bb1ba5e2a32d57ec4b3817e6de830
Siarhei Bokuts <siarheibokuts@gmail.com>
2025-08-13 12:39:28 +0200

    added docker compose for postgres; added config module; added typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getTypeOrmConfig } from './db/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getTypeOrmConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(private dataSource: DataSource) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async checkDatabase(): Promise<string> {
+    try {
+      await this.dataSource.query('SELECT 1');
+      return 'Database connection OK ✅';
+    } catch (err) {
+      console.error(err);
+      return 'Database connection FAILED ❌';
+    }
   }
 }
