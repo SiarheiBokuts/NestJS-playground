341ec51078bc93c9610d4ef4a21ad8cfd17e167a
Siarhei Bokuts <siarheibokuts@gmail.com>
2025-08-19 15:10:01 +0200

    added and learned validation pipe and class-validator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // removes any properties that are not in the DTO
+      forbidNonWhitelisted: true, // throws an error if extra properties are present
+      transform: false, // automatically transforms the incoming payload into an instance of the DTO class
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 4200);
 }
 bootstrap();
