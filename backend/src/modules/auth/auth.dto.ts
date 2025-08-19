a8aa9769520d8737407a5f89dbb010e5d13ba43c
Siarhei Bokuts <pl-sib@whiteawaygroup.com>
2025-08-19 15:10:01 +0200

    added and learned validation pipe and class-validator

--- backend/src/modules/auth/auth.dto.ts
+++ backend/src/modules/auth/auth.dto.ts
@@ -1,9 +1,19 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
 
 export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
