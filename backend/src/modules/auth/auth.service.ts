f879bf5eb6135614667735c2b6bf50b865ce85e1
Siarhei Bokuts <siarheibokuts@gmail.com>
2025-08-19 16:19:41 +0200

    sign up flow + bcrypt

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,13 +1,39 @@
-/* eslint-disable @typescript-eslint/require-await */
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { RegisterDto } from './auth.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/db/entities/user.entity';
+import { Repository } from 'typeorm';
+
+const SALT_ROUNDS = 12; // Define a constant for the salt rounds
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
   async register(dto: RegisterDto) {
-    // const hashedPassword = await bcrypt.hash(dto.password, 10);
-    const user = { email: dto.email, password: dto.password };
-    // this.users.push(user);
+    const existingUser = await this.userRepository.findOne({
+      where: { email: dto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, SALT_ROUNDS);
+    console.log(
+      'Registering user:',
+      dto.email,
+      'with hashed password:',
+      hashedPassword,
+    );
+    const user = { email: dto.email, password: hashedPassword };
+
+    await this.userRepository.save(user);
+
     return { email: user.email };
   }
 
