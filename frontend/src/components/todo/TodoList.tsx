e928db9ea62783338d17934c58520b8815bf435f
Siarhei Bokuts <siarheibokuts@gmail.com>
2025-08-25 14:26:55 +0200

    added todo info modal

--- frontend/src/components/todo/TodoList.tsx
+++ frontend/src/components/todo/TodoList.tsx
@@ -9,6 +9,8 @@ import { useState } from "react";
 import { CheckIcon } from "@heroicons/react/24/solid";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import TodoInfoModal from "./TodoInfoModal";
+import { Todo } from "@/types/todo";
 
 export default function TodoList() {
   const { token } = useAuth();
@@ -16,6 +18,11 @@ export default function TodoList() {
 
   const [newTask, setNewTask] = useState("");
 
+  // Todo Info Modal State
+  const [isTodoInfoModalOpen, setIsTodoModalOpen] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  // Mutations
   const addTaskMutation = useMutation({
     mutationFn: async (title: string) => {
       return addTodo({ title }, token!);
@@ -63,60 +70,74 @@ export default function TodoList() {
     return <p className="text-center py-4 text-red-500">Error loading tasks</p>;
 
   return (
-    <div>
-      <div className="flex gap-2 pb-2">
-        <input
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          type="text"
-          placeholder="Add new todo..."
-          className="flex-1 border px-3 py-2 rounded"
-        />
-        <button
-          onClick={() => addTaskMutation.mutate(newTask)}
-          disabled={addTaskMutation.isPending}
-          className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 cursor-pointer"
-        >
-          Add
-        </button>
+    <>
+      <div>
+        <div className="flex gap-2 pb-2">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            type="text"
+            placeholder="Add new todo..."
+            className="flex-1 border px-3 py-2 rounded"
+          />
+          <button
+            onClick={() => addTaskMutation.mutate(newTask)}
+            disabled={addTaskMutation.isPending}
+            className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 cursor-pointer"
+          >
+            Add
+          </button>
+        </div>
+
+        <ul className="flex flex-col gap-2">
+          {todos?.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex justify-between border p-2 rounded"
+            >
+              <span>{todo.title}</span>
+
+              <div className="flex">
+                {/* Complete */}
+
+                {todo.isCompleted ? null : (
+                  <button
+                    onClick={() => completeTaskMutation.mutate(todo.id)}
+                    className="text-green-500 hover:text-green-700 cursor-pointer flex items-center w-5 h-5 mr-2"
+                  >
+                    <CheckIcon className="w-5 h-5" />
+                  </button>
+                )}
+
+                {/* Info */}
+                <button
+                  onClick={() => {
+                    setSelectedTodo(todo);
+                    setIsTodoModalOpen(true);
+                  }}
+                  className="text-neutral-500 hover:text-neutral-700 cursor-pointer flex items-center w-5 h-5 mr-2"
+                >
+                  <InformationCircleIcon className="w-5 h-5" />
+                </button>
+
+                {/* Remove */}
+                <button
+                  onClick={() => removeTaskMutation.mutate(todo.id)}
+                  className="text-rose-500 hover:text-rose-700 cursor-pointer w-5 h-5"
+                >
+                  <TrashIcon className="w-5 h-5" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
 
-      <ul className="flex flex-col gap-2">
-        {todos?.map((todo) => (
-          <li key={todo.id} className="flex justify-between border p-2 rounded">
-            <span>{todo.title}</span>
-
-            <div className="flex">
-              {/* Complete */}
-
-              {todo.isCompleted ? null : (
-                <button
-                  onClick={() => completeTaskMutation.mutate(todo.id)}
-                  className="text-green-500 hover:text-green-700 cursor-pointer flex items-center w-5 h-5 mr-2"
-                >
-                  <CheckIcon className="w-5 h-5" />
-                </button>
-              )}
-
-              {/* Info */}
-              <button
-                onClick={() => {}}
-                className="text-neutral-500 hover:text-neutral-700 cursor-pointer flex items-center w-5 h-5 mr-2"
-              >
-                <InformationCircleIcon className="w-5 h-5" />
-              </button>
-
-              {/* Remove */}
-              <button
-                onClick={() => removeTaskMutation.mutate(todo.id)}
-                className="text-rose-500 hover:text-rose-700 cursor-pointer w-5 h-5"
-              >
-                <TrashIcon className="w-5 h-5" />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <TodoInfoModal
+        isOpen={isTodoInfoModalOpen}
+        todo={selectedTodo!}
+        onClose={() => setIsTodoModalOpen(false)}
+      />
+    </>
   );
 }
